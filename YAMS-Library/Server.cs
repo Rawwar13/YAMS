43513b07ac0baf465794505b47c4ca825c408f50
Richard Benson <me@richardbenson.co.uk>
2010-11-25 17:59:10 +0000

    Set up webserver and minecraft server classes

---
+++ YAMS-Library/Server.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace YAMS
+{
+    public static class Server
+    {
+        public static bool bolServerRunning = false;
+
+
+    }
+}
---
+++ YAMS-Library/WebServer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using HttpServer;
+//using HttpServer.Headers;
+using HttpListener = HttpServer.HttpListener;
+
+namespace YAMS
+{
+    public static class WebServer
+    {
+
+        public static void init()
+        {
+        }
+
+    }
+}
