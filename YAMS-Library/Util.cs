85be1830db06b2b124d2c732e26b8bea881bd2e7
Richard Benson <me@richardbenson.co.uk>
2010-11-25 17:58:49 +0000

    Java detection utilities

---
+++ YAMS-Library/Util.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Win32;
+
+namespace YAMS
+{
+    public static class Util
+    {
+
+        private static string strJRERegKey = "SOFTWARE\\JavaSoft\\Java Runtime Environment";
+        private static string strJDKRegKey = "SOFTWARE\\JavaSoft\\Java Development Kit";
+
+        public static bool HasJRE()
+        {
+            try
+            {
+                RegistryKey rk = Registry.LocalMachine;
+                RegistryKey subKey = rk.OpenSubKey(strJRERegKey);
+                if (subKey != null) return true;
+                else return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool HasJDK()
+        {
+            try
+            {
+                RegistryKey rk = Registry.LocalMachine;
+                RegistryKey subKey = rk.OpenSubKey(strJDKRegKey);
+                if (subKey != null) return true;
+                else return false;
+            }
+            catch
+            {
+                return false;
+            }
+        
+        }
+
+        public static string JavaVersion(string strType = "jre")
+        {
+            string strKey = "";
+            switch (strType)
+            {
+                case "jre":
+                    strKey = strJRERegKey;
+                    break;
+                case "jdk":
+                    strKey = strJDKRegKey;
+                    break;
+            }
+            RegistryKey rk = Registry.LocalMachine;
+            RegistryKey subKey = rk.OpenSubKey(strKey);
+            if (subKey != null) return subKey.GetValue("CurrentVersion").ToString();
+            else return "";
+        }
+
+        public static string JavaPath(string strType = "jre")
+        {
+            string strKey = "";
+            switch (strType)
+            {
+                case "jre":
+                    strKey = strJRERegKey;
+                    break;
+                case "jdk":
+                    strKey = strJDKRegKey;
+                    break;
+            }
+            strKey += "\\" + JavaVersion(strType);
+            RegistryKey rk = Registry.LocalMachine;
+            RegistryKey subKey = rk.OpenSubKey(strKey);
+            if (subKey != null) return subKey.GetValue("JavaHome").ToString() + "\\bin\\";
+            else return "";
+        }
+    }
+}
