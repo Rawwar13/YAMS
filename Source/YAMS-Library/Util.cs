5c530d8c8869d9aeb4367b10ef4b368b80fb8a21
Richard Benson <me@richardbenson.co.uk>
2010-11-29 18:22:48 +0000

    Set defaults in first run and functions to get internal and external addresses

--- Source/YAMS-Library/Util.cs
+++ Source/YAMS-Library/Util.cs
@@ -1,5 +1,8 @@
 ﻿using System.IO;
 using Microsoft.Win32;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.Text;
 
 namespace YAMS
 {
@@ -114,9 +117,73 @@ namespace YAMS
             //Grab latest server jar
             YAMS.AutoUpdate.UpdateIfNeeded(YAMS.AutoUpdate.strMCServerURL, strRootFolder + @"\lib\minecraft_server.jar.UPDATE");
 
+            //Set our MC Defaults in the DB
+            YAMS.Database.SaveSetting("admin-slot", "true", "MC");
+            YAMS.Database.SaveSetting("enable-health", "true", "MC");
+            YAMS.Database.SaveSetting("grow-trees", "true", "MC");
+            YAMS.Database.SaveSetting("hellworld", "false", "MC");
+            YAMS.Database.SaveSetting("level-name", @"..\\worlds\\world1", "MC");
+            YAMS.Database.SaveSetting("max-connections", "1", "MC");
+            YAMS.Database.SaveSetting("max-players", "20", "MC");
+            YAMS.Database.SaveSetting("motd", "Welcome to a YAMS server!", "MC");
+            YAMS.Database.SaveSetting("online-mode", "true", "MC");
+            YAMS.Database.SaveSetting("public", "false", "MC");
+            YAMS.Database.SaveSetting("pvp", "true", "MC");
+            YAMS.Database.SaveSetting("server-ip", "127.0.0.1", "MC");
+            YAMS.Database.SaveSetting("server-name", "My YAMS MC Server", "MC");
+            YAMS.Database.SaveSetting("server-port", "56552", "MC"); //Use an IANA legal internal port 49152 - 65535
+            YAMS.Database.SaveSetting("spawn-animals", "true", "MC");
+            YAMS.Database.SaveSetting("spawn-monsters", "true", "MC");
+            YAMS.Database.SaveSetting("verify-names", "true", "MC");
+
+            //Set our YAMS Defaults
+            YAMS.Database.SaveSetting("UpdateJAR", "true", "YAMS");
+            YAMS.Database.SaveSetting("UpdateSVC", "false", "YAMS");
+            YAMS.Database.SaveSetting("UpdateGUI", "true", "YAMS");
+            YAMS.Database.SaveSetting("UpdateWeb", "true", "YAMS");
+            YAMS.Database.SaveSetting("UpdateAddons", "true", "YAMS");
+            YAMS.Database.SaveSetting("RestartOnJarUpdate", "false", "YAMS");
+            YAMS.Database.SaveSetting("RestartOnSVCUpdate", "false", "YAMS");
+            YAMS.Database.SaveSetting("Memory", "1024", "YAMS");
+            YAMS.Database.SaveSetting("EnableJavaOptimisations", "true", "YAMS");
+            YAMS.Database.SaveSetting("ListenPort", "25565", "YAMS");
+            YAMS.Database.SaveSetting("ExternalIP", GetExternalIP().ToString(), "YAMS");
+            YAMS.Database.SaveSetting("ListenIP", GetListenIP().ToString(), "YAMS");
+
+
             //Tell the DB that we've run this
             YAMS.Database.SaveSetting("FirstRun", "true", "YAMS");
 
+        }
+
+        //Uses WhatIsMyIP.com to determine the user's external IP
+        public static IPAddress GetExternalIP()
+        {
+            string strExternalIPChecker = "http://whatismyip.com";
+            Regex regGetIP = new Regex(@"(?<=<TITLE>.*)\d*\.\d*\.\d*\.\d*(?=</TITLE>)");
+            WebClient wcGetIP = new WebClient();
+            UTF8Encoding utf8 = new UTF8Encoding();
+            string strResponse = "";
+            try
+            {
+                strResponse = utf8.GetString(wcGetIP.DownloadData(strExternalIPChecker));
+            }
+            catch (WebException e)
+            {
+                YAMS.Database.AddLog("Unable to determine external IP: " + e.Data, "utils", "warn");
+            }
+
+            Match regMatch = regGetIP.Match(strResponse);
+            IPAddress ipExternal = null;
+            if (regMatch.Success) ipExternal = IPAddress.Parse(regMatch.Value);
+            return ipExternal;
+        }
+
+        //Get an IP Address to bind to
+        public static IPAddress GetListenIP()
+        {
+            IPHostEntry ipListen = Dns.GetHostEntry(Dns.GetHostName());
+            return ipListen.AddressList[0];
         }
     }
 }
