d8f1c15d18eeacdb0a955ceaa9984ec90258474e
Richard Benson <me@richardbenson.co.uk>
2010-12-05 21:55:16 +0000

    New "Core" function, will do most calls to other libraries, should mean service only needs 1 line of non-service based code

---
+++ Source/YAMS-Library/Functions/Core.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.SqlServerCe;
+
+namespace YAMS
+{
+    public static class Core
+    {
+        public static string RootFolder = new System.IO.FileInfo(System.Reflection.Assembly.GetExecutingAssembly().Location).DirectoryName;
+
+        public static List<MCServer> Servers = new List<MCServer> { };
+
+        public static void StartUp()
+        {
+            //Start DB Connection
+            YAMS.Database.init();
+
+            //Is this the first run?
+            if (YAMS.Database.GetSetting("FirstRun", "YAMS") != "true") YAMS.Util.FirstRun();
+
+            //Check for updates
+            YAMS.AutoUpdate.CheckUpdates();
+
+            //Load any servers
+            SqlCeDataReader readerServers = YAMS.Database.GetServers();
+            while (readerServers.Read())
+            {
+                MCServer myServer = new MCServer(Convert.ToInt32(readerServers["ServerID"]));
+                if (Convert.ToBoolean(readerServers["ServerAutostart"])) myServer.Start();
+                Servers.Add(myServer);
+            }
+
+        }
+    }
+}
