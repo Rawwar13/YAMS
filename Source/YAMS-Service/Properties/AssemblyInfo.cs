dc95a9052751e4a2cec63a55cd52c40ac589fd37
Richard Benson <me@richardbenson.co.uk>
2011-01-14 17:55:19 +0000

    Add lib folder for dlls

--- Source/YAMS-Service/Properties/AssemblyInfo.cs
+++ Source/YAMS-Service/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.57.1359")]
-[assembly: AssemblyFileVersion("1.0.57.1359")]
+[assembly: AssemblyVersion("1.0.71.1744")]
+[assembly: AssemblyFileVersion("1.0.71.1744")]
