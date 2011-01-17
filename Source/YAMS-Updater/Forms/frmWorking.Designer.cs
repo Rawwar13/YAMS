91dc5e5a8d2e211ca62623a31de097aa0acac2e9
Richard Benson <me@richardbenson.co.uk>
2011-01-17 20:34:30 +0000

    Add a "working" progress dialog

---
+++ Source/YAMS-Updater/Forms/frmWorking.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace YAMS_Updater
+{
+    partial class frmWorking
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblCurrentItem = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Current Item: ";
+            this.label2.UseWaitCursor = true;
+            // 
+            // lblCurrentItem
+            // 
+            this.lblCurrentItem.AutoSize = true;
+            this.lblCurrentItem.Location = new System.Drawing.Point(90, 56);
+            this.lblCurrentItem.Name = "lblCurrentItem";
+            this.lblCurrentItem.Size = new System.Drawing.Size(16, 13);
+            this.lblCurrentItem.TabIndex = 2;
+            this.lblCurrentItem.Text = "...";
+            // 
+            // frmWorking
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(449, 136);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblCurrentItem);
+            this.Controls.Add(this.label2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmWorking";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Working...";
+            this.UseWaitCursor = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        public System.Windows.Forms.Label lblCurrentItem;
+    }
+}
