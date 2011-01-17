bad553b1239372556726611a7955f3d26c8eee7c
Richard Benson <me@richardbenson.co.uk>
2011-01-17 22:43:37 +0000

    * Move start and stop service to functions
    * Remove DownloadURL function
    * Request UAC access if we don't have it

--- Source/YAMS-Updater/Program.cs
+++ Source/YAMS-Updater/Program.cs
@@ -7,6 +7,7 @@ using System.ServiceProcess;
 using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using YAMS;
+using System.Security.Principal;
 
 namespace YAMS_Updater
 {
@@ -22,81 +23,46 @@ namespace YAMS_Updater
         
         static void Main(string[] args)
         {
+            //We need Admin for almost everything in here
+            // Needs UAC elevation for webmin to run
+            WindowsPrincipal principal = new WindowsPrincipal(WindowsIdentity.GetCurrent());
+            bool hasAdministrativeRight = principal.IsInRole(WindowsBuiltInRole.Administrator);
+
+            if (!hasAdministrativeRight)
+            {
+                ProcessStartInfo processInfo = new ProcessStartInfo();
+                processInfo.Verb = "runas";
+                processInfo.FileName = Application.ExecutablePath;
+                if (args.Length > 0 ) processInfo.Arguments = args.ToString();
+                try
+                {
+                    Process.Start(processInfo);
+                }
+                catch
+                {
+                    Environment.Exit(0);
+                }
+                Environment.Exit(0);
+            }
+            
             Console.WriteLine("*** YAMS Updater ***");
 
             if (args.Contains<string>("/restart"))
             {
                 //We're just here to restart the service safely after some updates
                 Console.WriteLine("Restarting service...");
-                ServiceController scYAMS = new ServiceController("YAMS_Service");
-                scYAMS.Stop();
-                while (!scYAMS.Status.Equals(ServiceControllerStatus.Stopped))
-                {
-                    scYAMS.Refresh();
-                }
-                Console.WriteLine("Service stopped");
-
-                //Apply any updates to core files, these should cope with any other updates
-                if (File.Exists(RootFolder + @"\YAMS-Library.dll.UPDATE"))
-                {
-                    File.Move(RootFolder + @"\YAMS-Library.dll", RootFolder + @"\YAMS-Library.dll.OLD");
-                    File.Move(RootFolder + @"\YAMS-Library.dll.UPDATE", RootFolder + @"\YAMS-Library.dll");
-                    File.Delete(RootFolder + @"\YAMS-Library.dll.OLD");
-                }
-                if (File.Exists(RootFolder + @"\YAMS-Service.exe.UPDATE"))
-                {
-                    File.Move(RootFolder + @"\YAMS-Service.exe", RootFolder + @"\YAMS-Service.exe.OLD");
-                    File.Move(RootFolder + @"\YAMS-Service.exe.UPDATE", RootFolder + @"\YAMS-Service.exe");
-                    File.Delete(RootFolder + @"\Service.exe.OLD");
-                }
-
-                //Restart the service
-                scYAMS.Start();
-                Console.WriteLine("Service started");
-
+                StopService();
+                StartService();
                 Environment.Exit(0);
             }
             else if (args.Contains<string>("/start"))
             {
-                ServiceController scYAMS = new ServiceController("YAMS_Service");
-                if (!scYAMS.Status.Equals(ServiceControllerStatus.Stopped))
-                {
-                    Console.WriteLine("Service already running");
-                }
-                else
-                {
-                    //Apply any updates to core files, these should cope with any other updates
-                    if (File.Exists(RootFolder + @"\YAMS-Library.dll.UPDATE"))
-                    {
-                        File.Move(RootFolder + @"\YAMS-Library.dll", RootFolder + @"\YAMS-Library.dll.OLD");
-                        File.Move(RootFolder + @"\YAMS-Library.dll.UPDATE", RootFolder + @"\YAMS-Library.dll");
-                        File.Delete(RootFolder + @"\YAMS-Library.dll.OLD");
-                    }
-                    if (File.Exists(RootFolder + @"\YAMS-Service.exe.UPDATE"))
-                    {
-                        File.Move(RootFolder + @"\YAMS-Service.exe", RootFolder + @"\YAMS-Service.exe.OLD");
-                        File.Move(RootFolder + @"\YAMS-Service.exe.UPDATE", RootFolder + @"\YAMS-Service.exe");
-                        File.Delete(RootFolder + @"\Service.exe.OLD");
-                    }
-
-                    //Restart the service
-                    scYAMS.Start();
-                    Console.WriteLine("Service started");
-                }
+                StartService();
                 Environment.Exit(0);
             }
             else if (args.Contains<string>("/stop"))
             {
-                ServiceController scYAMS = new ServiceController("YAMS_Service");
-                if (scYAMS.Status.Equals(ServiceControllerStatus.Stopped))
-                {
-                    Console.WriteLine("Service not running");
-                }
-                else
-                {
-                    scYAMS.Stop();
-                    Console.WriteLine("Service stopped");
-                }
+                StopService();
                 Environment.Exit(0);
             }
             else
@@ -109,7 +75,6 @@ namespace YAMS_Updater
                 catch { }
 
                 System.Windows.Forms.Application.EnableVisualStyles();
-                //Application.SetCompatibleTextRenderingDefault(false);
 
                 YAMS.Database.init();
                 YAMS.Database.AddLog("Updater Starting");
@@ -127,50 +92,49 @@ namespace YAMS_Updater
         
         }
 
-        private static bool DownloadURLToFile(string strURL, string strFile)
+        public static void StopService()
         {
-            try
+            ServiceController scYAMS = new ServiceController("YAMS_Service");
+            if (scYAMS.Status.Equals(ServiceControllerStatus.Stopped))
             {
-                //Set up a request and include our eTag
-                HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create(strURL);
-                request.Method = "GET";
-
-                //Grab the response
-                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-
-                //Stream the file
-                Stream strm = response.GetResponseStream();
-                FileStream fs = new FileStream(strFile, FileMode.Create, FileAccess.Write, FileShare.None);
-                const int ArrSize = 10000;
-                Byte[] barr = new Byte[ArrSize];
-                while (true)
-                {
-                    int Result = strm.Read(barr, 0, ArrSize);
-                    if (Result == -1 || Result == 0)
-                        break;
-                    fs.Write(barr, 0, Result);
-                }
-                fs.Flush();
-                fs.Close();
-                strm.Close();
-                response.Close();
-
-                Console.WriteLine("Downloaded: " + strURL);
-
-                return true;
+                Console.WriteLine("Service not running");
             }
-            catch (System.Net.WebException ex)
+            else
             {
-                if (ex.Response != null)
-                {
-                    using (HttpWebResponse response = ex.Response as HttpWebResponse)
-                    {
-                        Console.WriteLine(string.Format("Failed to fetch " + strURL + ". Error Code: {0}", response.StatusCode));
-                        return false;
-                    }
-                }
-                else return false;
+                scYAMS.Stop();
+                Console.WriteLine("Service stopped");
             }
+
+        }
+
+        public static void StartService()
+        {
+            ServiceController scYAMS = new ServiceController("YAMS_Service");
+            if (!scYAMS.Status.Equals(ServiceControllerStatus.Stopped))
+            {
+                Console.WriteLine("Service already running");
+            }
+            else
+            {
+                //Apply any updates to core files, these should cope with any other updates
+                if (File.Exists(RootFolder + @"\YAMS-Library.dll.UPDATE"))
+                {
+                    File.Move(RootFolder + @"\YAMS-Library.dll", RootFolder + @"\YAMS-Library.dll.OLD");
+                    File.Move(RootFolder + @"\YAMS-Library.dll.UPDATE", RootFolder + @"\YAMS-Library.dll");
+                    File.Delete(RootFolder + @"\YAMS-Library.dll.OLD");
+                }
+                if (File.Exists(RootFolder + @"\YAMS-Service.exe.UPDATE"))
+                {
+                    File.Move(RootFolder + @"\YAMS-Service.exe", RootFolder + @"\YAMS-Service.exe.OLD");
+                    File.Move(RootFolder + @"\YAMS-Service.exe.UPDATE", RootFolder + @"\YAMS-Service.exe");
+                    File.Delete(RootFolder + @"\Service.exe.OLD");
+                }
+
+                //Restart the service
+                scYAMS.Start();
+                Console.WriteLine("Service started");
+            }
+
         }
     }
 }
