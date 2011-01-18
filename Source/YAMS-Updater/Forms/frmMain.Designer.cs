45e29c3d684785ea3a952d97155e43f55624ddab
Richard Benson <me@richardbenson.co.uk>
2011-01-18 22:49:26 +0000

    Service control dialog

--- Source/YAMS-Updater/Forms/frmMain.Designer.cs
+++ Source/YAMS-Updater/Forms/frmMain.Designer.cs
@@ -28,33 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.timStatus = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // groupBox1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(104, 97);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(30, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Main";
+            this.groupBox1.Controls.Add(this.btnStop);
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Controls.Add(this.lblStatus);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(169, 65);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Service Control";
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(88, 32);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Enabled = false;
+            this.btnStart.Location = new System.Drawing.Point(7, 32);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Dock = System.Windows.Forms.DockStyle.Right;
+            this.lblStatus.Location = new System.Drawing.Point(75, 16);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(91, 13);
+            this.lblStatus.TabIndex = 0;
+            this.lblStatus.Text = "checking status...";
+            this.lblStatus.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
             // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(192, 90);
+            this.Controls.Add(this.groupBox1);
             this.Name = "frmMain";
-            this.Text = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "YAMS Control";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Timer timStatus;
+
     }
 }
--- Source/YAMS-Updater/Forms/frmMain.cs
+++ Source/YAMS-Updater/Forms/frmMain.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.ServiceProcess;
 
 namespace YAMS_Updater
 {
@@ -14,6 +15,70 @@ namespace YAMS_Updater
         public frmMain()
         {
             InitializeComponent();
+
+            timStatus.Tick += new EventHandler(timStatus_Tick);
+            timStatus.Start();
         }
+
+        void timStatus_Tick(object sender, EventArgs e)
+        {
+            ServiceController svcYAMS = new ServiceController("YAMS_Service");
+
+            switch (svcYAMS.Status) {
+                case ServiceControllerStatus.Stopped:
+                    lblStatus.Text = "Stopped";
+                    btnStop.Enabled = false;
+                    btnStart.Enabled = true;
+                    break;
+                case ServiceControllerStatus.Running:
+                    lblStatus.Text = "Running";
+                    btnStart.Enabled = false;
+                    btnStop.Enabled = true;
+                    break;
+                case ServiceControllerStatus.Paused:
+                    lblStatus.Text = "Paused";
+                    btnStop.Enabled = true;
+                    btnStart.Enabled = true;
+                    break;
+                case ServiceControllerStatus.StartPending:
+                    lblStatus.Text = "Starting";
+                    btnStart.Enabled = false;
+                    btnStop.Enabled = false;
+                    break;
+                case ServiceControllerStatus.StopPending:
+                    lblStatus.Text = "Stopping";
+                    btnStart.Enabled = false;
+                    btnStop.Enabled = false;
+                    break;
+                case ServiceControllerStatus.PausePending:
+                    lblStatus.Text = "Pausing";
+                    btnStart.Enabled = false;
+                    btnStop.Enabled = false;
+                    break;
+                case ServiceControllerStatus.ContinuePending:
+                    lblStatus.Text = "Continuing";
+                    btnStart.Enabled = false;
+                    btnStop.Enabled = false;
+                    break;
+                default:
+                    lblStatus.Text = "Unknown";
+                    btnStart.Enabled = false;
+                    btnStop.Enabled = false;
+                    break;
+            }
+            this.Refresh();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Program.StartService();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            Program.StopService();
+        }
+
+        
     }
 }
