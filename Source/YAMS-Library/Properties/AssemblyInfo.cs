570f954e7c02c2de192f21e00ad5d8a32cfebf5a
Richard Benson <me@richardbenson.co.uk>
2011-02-13 22:40:07 +0000

    Version bumps

--- Source/YAMS-Library/Properties/AssemblyInfo.cs
+++ Source/YAMS-Library/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.182.1225")]
-[assembly: AssemblyFileVersion("0.1.182.1225")]
+[assembly: AssemblyVersion("0.1.190.2215")]
+[assembly: AssemblyFileVersion("0.1.190.2215")]
