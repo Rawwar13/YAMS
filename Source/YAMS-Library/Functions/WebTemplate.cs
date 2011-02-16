4db657387e0c66ff76c47e2d99b8e0fc20d370ad
Richard Benson <me@richardbenson.co.uk>
2011-02-16 18:15:06 +0000

    Moved template code into own module

---
+++ Source/YAMS-Library/Functions/WebTemplate.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace YAMS
+{
+    public class WebTemplate
+    {
+        private static string OpenTag = @"\{\?Y\:";
+        private static string CloseTag = @"\?\}";
+        private static Regex TagFinder = new Regex("(" + OpenTag + ")([^]+)(" + CloseTag + ")");
+
+        public static string ReplaceTags(string strInput, Dictionary<string, string> dicTags)
+        {
+            string strOutput = strInput;
+
+            MatchCollection results = TagFinder.Matches(strInput);
+            foreach (Match match in results)
+            {
+                Regex replacer = new Regex(OpenTag + match.Value + CloseTag);
+                if (dicTags.ContainsKey(match.Value)) strOutput = replacer.Replace(strOutput, dicTags[match.Value]);
+            }
+
+            return strOutput;
+        }
+
+    }
+}
