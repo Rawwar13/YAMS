69b4ba3f5403b0d54a462387b42d01f192e2210b
Richard Benson <me@richardbenson.co.uk>
2011-02-16 18:16:59 +0000

    Sorting out build patterns and fixing up Visual Studio stuff, plus build increments

--- Source/YAMS-Updater/Properties/AssemblyInfo.cs
+++ Source/YAMS-Updater/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.19.2215")]
-[assembly: AssemblyFileVersion("0.1.19.2215")]
+[assembly: AssemblyVersion("0.1.56.1806")]
+[assembly: AssemblyFileVersion("0.1.56.1806")]
