6b0120c45c8f2ec398446ca2422ac61fe215135d
Richard Benson <me@richardbenson.co.uk>
2011-02-18 12:13:39 +0000

    Borked the creation of the class with a bad regex

--- Source/YAMS-Library/Functions/WebTemplate.cs
+++ Source/YAMS-Library/Functions/WebTemplate.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -10,17 +9,18 @@ namespace YAMS
     {
         private static string OpenTag = @"\{\?Y\:";
         private static string CloseTag = @"\?\}";
-        private static Regex TagFinder = new Regex("(" + OpenTag + ")([^]+)(" + CloseTag + ")");
+        private static Regex TagFinder;
 
-        public static string ReplaceTags(string strInput, Dictionary<string, string> dicTags)
+        public static string ReplaceTags(string strInput, Dictionary<string, string> dicInputTags)
         {
+            TagFinder = new Regex(@"(" + OpenTag + @")([^}]+)(" + CloseTag + @")");
             string strOutput = strInput;
 
             MatchCollection results = TagFinder.Matches(strInput);
             foreach (Match match in results)
             {
-                Regex replacer = new Regex(OpenTag + match.Value + CloseTag);
-                if (dicTags.ContainsKey(match.Value)) strOutput = replacer.Replace(strOutput, dicTags[match.Value]);
+                Regex replacer = new Regex(OpenTag + match.Groups[2].Value + CloseTag);
+                if (dicInputTags.ContainsKey(match.Groups[2].Value)) strOutput = replacer.Replace(strOutput, dicInputTags[match.Groups[2].Value]);
             }
 
             return strOutput;
