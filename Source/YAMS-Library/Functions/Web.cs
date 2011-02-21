1f9bae3eeca2be78aafdd9318d3ef293bb6060bb
Richard Benson <me@richardbenson.co.uk>
2011-02-21 22:32:24 +0000

    Split admin and public web servers to different ports and instances, also only try restarting for up to 2 minutes in the case that the port is in use

--- Source/YAMS-Library/Functions/Web.cs
+++ Source/YAMS-Library/Functions/Web.cs
@@ -22,9 +22,11 @@ namespace YAMS
 {
     public static class WebServer
     {
-        private static Server myServer;
+        private static Server adminServer;
+        private static Server publicServer;
 
-        private static Thread serverThread;
+        private static Thread adminServerThread;
+        private static Thread publicServerThread;
 
         //Control
         public static void Init()
@@ -38,31 +40,38 @@ namespace YAMS
                 File.Delete(Core.RootFolder + @"\web.zip");
             }
             
-            myServer = new Server();
+            adminServer = new Server();
+            publicServer = new Server();
 
             //Handle the requests for static files
-            var module = new FileModule();
-            module.Resources.Add(new FileResources("/assets/", YAMS.Core.RootFolder + "\\web\\assets\\"));
+            var adminModule = new FileModule();
+            adminModule.Resources.Add(new FileResources("/assets/", YAMS.Core.RootFolder + "\\web\\assets\\"));
+            adminServer.Add(adminModule);
 
             //Add any server specific folders
+            var publicModule = new FileModule();
+            publicModule.Resources.Add(new FileResources("/assets/", YAMS.Core.RootFolder + "\\web\\assets\\"));
             SqlCeDataReader readerServers = YAMS.Database.GetServers();
             while (readerServers.Read())
             {
                 var intServerID = readerServers["ServerID"].ToString();
-                module.Resources.Add(new FileResources("/servers/" + intServerID + "/map/", YAMS.Core.RootFolder + "\\servers\\" + intServerID + "\\renders\\gmap\\output\\"));
-                module.Resources.Add(new FileResources("/servers/" + intServerID + "/renders/", YAMS.Core.RootFolder + "\\servers\\" + intServerID + "\\renders\\"));
+                publicModule.Resources.Add(new FileResources("/servers/" + intServerID + "/map/", YAMS.Core.RootFolder + "\\servers\\" + intServerID + "\\renders\\gmap\\output\\"));
+                publicModule.Resources.Add(new FileResources("/servers/" + intServerID + "/renders/", YAMS.Core.RootFolder + "\\servers\\" + intServerID + "\\renders\\"));
             }
-            myServer.Add(module);
+            publicServer.Add(publicModule);
 
             //Handle requests to API
-            myServer.Add(new API());
+            adminServer.Add(new AdminAPI());
+            publicServer.Add(new PublicAPI());
 
-            myServer.Add(HttpListener.Create(IPAddress.Any, Convert.ToInt32(YAMS.Database.GetSetting("ListenPort", "YAMS"))));
-            //myServer.RequestReceived += new EventHandler<RequestEventArgs>(RequestReceived);
+            adminServer.Add(HttpListener.Create(IPAddress.Any, Convert.ToInt32(YAMS.Database.GetSetting("AdminListenPort", "YAMS"))));
+            publicServer.Add(HttpListener.Create(IPAddress.Any, Convert.ToInt32(YAMS.Database.GetSetting("PublicListenPort", "YAMS"))));
 
-            myServer.ErrorPageRequested += new EventHandler<ErrorPageEventArgs>(myServer_ErrorPageRequested);
+            adminServer.ErrorPageRequested += new EventHandler<ErrorPageEventArgs>(myServer_ErrorPageRequested);
+            publicServer.ErrorPageRequested += new EventHandler<ErrorPageEventArgs>(myServer_ErrorPageRequested);
 
-            serverThread = new Thread(new ThreadStart(Start));
+            adminServerThread = new Thread(new ThreadStart(StartAdmin));
+            publicServerThread = new Thread(new ThreadStart(StartPublic));
         }
 
         static void myServer_ErrorPageRequested(object sender, ErrorPageEventArgs e)
@@ -74,38 +83,110 @@ namespace YAMS
             e.Response.Body.Write(buffer, 0, buffer.Length);
         }
 
-        public static void Start()
+        public static void StartAdmin()
         {
+            int intTryCount = 0;
             try
             {
-                myServer.Start(5);
-
+                adminServer.Start(5);
                 //Start our session provider
-                WebSession.Start(myServer);
+                WebSession.Start(adminServer);
             }
             catch (System.Net.Sockets.SocketException e)
             {
                 //Previous service has not released the port, so hang on and try again.
+                intTryCount++;
+                Database.AddLog("Admin Web server port still in use, attempt " + intTryCount, "web", "warn");
                 Thread.Sleep(1000);
-                Start();
+                if (intTryCount < 120)
+                {
+                    StartAdmin();
+                }
+                else
+                {
+                    EventLog myLog = new EventLog();
+                    myLog.Source = "YAMS";
+                    myLog.WriteEntry("Unable to start admin web server", EventLogEntryType.Error);
+                }
             }
             catch (Exception e) {
                 EventLog myLog = new EventLog();
                 myLog.Source = "YAMS";
                 myLog.WriteEntry("Exception: " + e.Data, EventLogEntryType.Error);
             }
+            
+            //try {
+            //    Networking.OpenFirewallPort(Convert.ToInt32(YAMS.Database.GetSetting("AdminListenPort", "YAMS")), "YAMS Admin Web Server");
+            //    Networking.OpenUPnP(Convert.ToInt32(YAMS.Database.GetSetting("AdminListenPort", "YAMS")), "YAMS Admin Web Server");
+            //} catch (Exception e) {
+            //    YAMS.Database.AddLog(e.Message);
+            //}
+
 
         }
 
+        public static void StartPublic()
+        {
+            int intTryCount = 0;
+            try
+            {
+                publicServer.Start(10);
+            }
+            catch (System.Net.Sockets.SocketException e)
+            {
+                //Previous service has not released the port, so hang on and try again.
+                intTryCount++;
+                Database.AddLog("Public Web server port still in use, attempt " + intTryCount, "web", "warn");
+                Thread.Sleep(1000);
+                if (intTryCount < 120)
+                {
+                    StartPublic();
+                }
+                else
+                {
+                    EventLog myLog = new EventLog();
+                    myLog.Source = "YAMS";
+                    myLog.WriteEntry("Unable to start public web server", EventLogEntryType.Error);
+                }
+            }
+            catch (Exception e)
+            {
+                EventLog myLog = new EventLog();
+                myLog.Source = "YAMS";
+                myLog.WriteEntry("Exception: " + e.Data, EventLogEntryType.Error);
+            }
+
+            //try {
+            //    Networking.OpenFirewallPort(Convert.ToInt32(YAMS.Database.GetSetting("PublicListenPort", "YAMS")), "YAMS Public Web Server");
+            //    Networking.OpenUPnP(Convert.ToInt32(YAMS.Database.GetSetting("PublicListenPort", "YAMS")), "YAMS Public Web Server");
+            //} catch (Exception e) {
+            //    YAMS.Database.AddLog(e.Message);
+            //}
+        
+        }
+
+
         public static void Stop()
         {
-            serverThread.Abort();
+            //try
+            //{
+            //    Networking.CloseFirewallPort(Convert.ToInt32(YAMS.Database.GetSetting("AdminListenPort", "YAMS")));
+            //    Networking.CloseUPnP(Convert.ToInt32(YAMS.Database.GetSetting("AdminListenPort", "YAMS")));
+            //    Networking.CloseFirewallPort(Convert.ToInt32(YAMS.Database.GetSetting("PublicListenPort", "YAMS")));
+            //    Networking.CloseUPnP(Convert.ToInt32(YAMS.Database.GetSetting("PublicListenPort", "YAMS")));
+            //}
+            //catch (Exception e)
+            //{
+            //    YAMS.Database.AddLog(e.Message);
+            //}
+            adminServerThread.Abort();
+            publicServerThread.Abort();
         }
 
     }
 
 
-    public class API : IModule
+    public class AdminAPI : IModule
     {
         public ProcessingResult Process(RequestContext context)
         {
@@ -164,25 +245,17 @@ namespace YAMS
                             }
                             strResponse += "]}";
                             break;
-                        //case "players":
-                        //    //Get status of a server
+                        //case "gmap":
+                        //    //Maps a server
                         //    s = Core.Servers[Convert.ToInt32(context.Request.Parameters["serverid"])];
-                        //    strResponse = "{ \"serverid\" : " + intServerID + ",";
-                        //    strResponse += "\"players\" : [";
-                        //    if (s.Players.Count > 0)
-                        //    {
-                        //        s.Players.ForEach(delegate(string p)
-                        //        {
-                        //            strResponse += "\"" + p + "\",";
-                        //        });
-                        //        strResponse = strResponse.Remove(strResponse.Length - 1);
-                        //    }
-                        //    strResponse += "]}";
+                        //    AddOns.Overviewer gmap = new AddOns.Overviewer(s);
+                        //    gmap.Start();
+                        //    strResponse = "{ \"result\" : \"sent\" }";
                         //    break;
                         case "gmap":
                             //Maps a server
                             s = Core.Servers[Convert.ToInt32(context.Request.Parameters["serverid"])];
-                            AddOns.Overviewer gmap = new AddOns.Overviewer(s);
+                            AddOns.c10t gmap = new AddOns.c10t(s);
                             gmap.Start();
                             strResponse = "{ \"result\" : \"sent\" }";
                             break;
@@ -297,68 +370,80 @@ namespace YAMS
                     return ProcessingResult.SendResponse;
                 }
             }
+            else {
+                return ProcessingResult.Abort;
+            }
+
+        }
+
+    }
+
+    public class PublicAPI : IModule
+    {
+        public ProcessingResult Process(RequestContext context)
+        {
+
+            //it's a public request, work out what they want
+            // / = list servers
+            // /[0-9]+/ = server home page including chat log
+            // /[0-9]+/map = Google Map
+            // /[0-9]+/renders = c10t renders
+
+            Regex regRoot = new Regex(@"^/$");
+            Regex regServerList = new Regex(@"^/servers/$");
+            Regex regServerHome = new Regex(@"^/servers/([0-9]+)/$");
+            Regex regServerGMap = new Regex(@"^/servers/([0-9]+)/map/");
+            Regex regServerRenders = new Regex(@"^/servers/([0-9]+)/renders/");
+
+            if (regServerGMap.Match(context.Request.Uri.AbsolutePath).Success || regServerRenders.Match(context.Request.Uri.AbsolutePath).Success)
+            {
+                return ProcessingResult.Continue;
+            }
             else
             {
-                //it's a public request, work out what they want
-                // / = list servers
-                // /[0-9]+/ = server home page including chat log
-                // /[0-9]+/map = Google Map
-                // /[0-9]+/renders = c10t renders
+                string strTemplate = "No matching Template";
+                Dictionary<string, string> dicTags = new Dictionary<string, string>();
 
-                Regex regRoot = new Regex(@"^/$");
-                Regex regServerList = new Regex(@"^/servers/$");
-                Regex regServerHome = new Regex(@"^/servers/([0-9]+)/$");
-                Regex regServerGMap = new Regex(@"^/servers/([0-9]+)/map/");
-                Regex regServerRenders = new Regex(@"^/servers/([0-9]+)/renders/");
-
-                if (regServerGMap.Match(context.Request.Uri.AbsolutePath).Success || regServerRenders.Match(context.Request.Uri.AbsolutePath).Success)
+                if (regRoot.Match(context.Request.Uri.AbsolutePath).Success)
                 {
-                    return ProcessingResult.Continue;
+                    //Server Root
+                    strTemplate = File.ReadAllText(Core.RootFolder + @"\web\templates\root.html");
+                    dicTags.Add("PageTitle", "YAMS Hosted Server");
+                    dicTags.Add("PageBody", "test");
+                }
+                else if (regServerList.Match(context.Request.Uri.AbsolutePath).Success)
+                {
+                    //List of Servers
+                    strTemplate = File.ReadAllText(Core.RootFolder + @"\web\templates\server-list.html");
+                    dicTags.Add("PageTitle", "Server List");
+                    dicTags.Add("PageBody", "test");
+                }
+                else if (regServerHome.Match(context.Request.Uri.AbsolutePath).Success)
+                {
+                    //Individual Server home
+                    strTemplate = File.ReadAllText(Core.RootFolder + @"\web\templates\server-home.html");
+                    dicTags.Add("PageTitle", "Server Home");
+                    dicTags.Add("PageBody", "test");
                 }
                 else
                 {
-                    string strTemplate = "No matching Template";
-                    Dictionary<string, string> dicTags = new Dictionary<string, string>();
-
-                    if (regRoot.Match(context.Request.Uri.AbsolutePath).Success)
-                    {
-                        //Server Root
-                        strTemplate = File.ReadAllText(Core.RootFolder + @"\web\templates\root.html");
-                        dicTags.Add("PageTitle", "YAMS Hosted Server");
-                    }
-                    else if (regServerList.Match(context.Request.Uri.AbsolutePath).Success)
-                    {
-                        //List of Servers
-                        strTemplate = File.ReadAllText(Core.RootFolder + @"\web\templates\server-list.html");
-                        dicTags.Add("PageTitle", "Server List");
-                    }
-                    else if (regServerHome.Match(context.Request.Uri.AbsolutePath).Success)
-                    {
-                        //Individual Server home
-                        strTemplate = File.ReadAllText(Core.RootFolder + @"\web\templates\server-home.html");
-                        dicTags.Add("PageTitle", "Server Home");
-                    }
-                    else
-                    {
-                        //Unknown
-                        return ProcessingResult.Abort;
-                    }
-
-                    dicTags.Add("PageBody", "test");
-                    
-                    //Run through our replacer
-                    strTemplate = WebTemplate.ReplaceTags(strTemplate, dicTags);
-
-                    //And send to the browser
-                    context.Response.Reason = "Completed - YAMS";
-                    context.Response.Connection.Type = ConnectionType.Close;
-                    byte[] buffer = Encoding.UTF8.GetBytes(strTemplate);
-                    context.Response.Body.Write(buffer, 0, buffer.Length);
-                    return ProcessingResult.SendResponse;
+                    //Unknown
+                    context.Response.Status = HttpStatusCode.NotFound;
+                    strTemplate = File.ReadAllText(Core.RootFolder + @"\web\templates\server-home.html");
+                    dicTags.Add("PageTitle", "404");
+                    dicTags.Add("PageBody", "<h1>404 - Not Found</h1>");
                 }
 
-            }
+                //Run through our replacer
+                strTemplate = WebTemplate.ReplaceTags(strTemplate, dicTags);
 
+                //And send to the browser
+                context.Response.Reason = "Completed - YAMS";
+                context.Response.Connection.Type = ConnectionType.Close;
+                byte[] buffer = Encoding.UTF8.GetBytes(strTemplate);
+                context.Response.Body.Write(buffer, 0, buffer.Length);
+                return ProcessingResult.SendResponse;
+            }
 
         }
 
