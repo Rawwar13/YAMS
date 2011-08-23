d5cbe374115c45ec74914e222eaeb3ba74cda4c5
Richard Benson <me@richardbenson.co.uk>
2011-08-23 14:05:03 +0100

    Make SendMessage it's own function and use that for sending MOTD to user.

--- Source/YAMS-Library/Objects/Player.cs
+++ Source/YAMS-Library/Objects/Player.cs
@@ -34,7 +34,12 @@ namespace YAMS.Objects
             if (Util.SearchFile(s.ServerDirectory + "ops.txt", strName)) this.Level = "op";
 
             //Emulate MOTD
-            if (Database.GetSetting("motd", "MC", this.Server.ServerID) != "") this.Server.Whisper(this.Username, Database.GetSetting("motd", "MC", this.Server.ServerID));
+            if (Database.GetSetting("motd", "MC", this.Server.ServerID) != "") this.SendMessage(Database.GetSetting("motd", "MC", this.Server.ServerID));
+        }
+
+        public void SendMessage(string strMessage)
+        {
+            this.Server.Whisper(this.Username, strMessage);
         }
 
     }
