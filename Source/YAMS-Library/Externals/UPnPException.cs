86613f65521bc287a27afd050848cba9aeb15112
Richard Benson <me@richardbenson.co.uk>
2011-09-16 17:22:48 +0100

    Add back in references for our networking objects, remove the broken interop and replace DLL version of SharpUPnP with an included class.

---
+++ Source/YAMS-Library/Externals/UPnPException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharpUPnP
+{
+    public class UPnPException
+        : Exception
+    {
+        public UPnPException(String message)
+            : base(message)
+        {
+
+        }
+    }
+}
